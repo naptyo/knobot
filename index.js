a50b090157c6d6922ec6cf8cc582dc25b03658fd
afian <afian@adrena.id>
2019-05-24 14:16:48 +0700

    bot reply functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import Discord from 'discord.js';
-
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content == '!ping') {
+        message.channel.send('Pong.');
+    }
 });
 
 client.login('NTgxMDU3MjgyODE3NjU0ODA1.XOZ01w.GMgpcD7PVFaZMngYtW8gdR85xng');
